e61ef56182389a99db6fe7f7a9930133d821aac2
Umema_Sultan <umemasultan11@gmail.com>
2026-06-07 00:28:15 +0500

    Initial commit

---
+++ arrow function/app.js
@@ -0,0 +1,151 @@
+// Question 1
+
+
+// function add(a, b) {
+//   return a + b;
+// }
+// // Answer:
+// let add = (a, b) => a + b;
+
+// console.log(add(2, 3)); 
+
+// Question 2
+
+// function greet(name) {
+//   return "Hello, " + name + "!";
+// }
+// // Answer:
+// let greet = name => "Hello, " + name + "!";
+// console.log(greet("Alice"));
+
+// Question 3
+
+// function square(x) {
+//     return x * x;
+// }
+// // Answer:
+// let square = x => x * x;
+// console.log(square(5));
+
+// Question 4
+
+// function isEven(num) {
+//     return num % 2 === 0;
+// }
+// // Answer:
+// let isEven = num => num % 2 === 0;
+// console.log(isEven(4));
+// console.log(isEven(7));
+
+// Question 5
+
+// function getFullName(firstName, lastName) {
+//     return firstName + " " + lastName;
+// }
+// // Answer:
+// let getFullName = (firstName, lastName) => firstName + " " + lastName;
+// console.log(getFullName("John", "Doe"));
+
+// Question 6
+// // let num = [1,2,3,4,5,6,7,8,9];
+
+// // let count = (arr) => {
+// //     console.log(arr.length);
+// // }
+
+// // count(num);
+
+// Question 7
+// // let num = [1,2,3,4,5,6,7,8,9];
+
+// // let evenNumber = (arr) => {
+// //     for(let i = 0; i < arr.length; i++){
+// //         if(arr[i] % 2 === 0){
+// //              console.log(arr[i]);
+// //         }
+// //     }
+// // }
+
+// // evenNumber(num);
+
+//  Question 8
+// // let num = [1,2,3,4,5,6,7,8,9];
+
+// // let oddNumber = (arr) => {
+// //     for(let i = 0; i < arr.length; i++){
+// //         if(arr[i] % 3 === 0){
+// //              console.log(arr[i]);
+// //         }
+// //     }
+// // }
+
+// // oddNumber(num);
+
+// Question 9
+// // let num = [1,2,3,4,10,6,7,8,9];
+
+// // let largest = (arr) => {
+// //     let max = arr[0];
+// //     for(let i = 1; i < arr.length; i++){
+// //         if(arr[i] > max){
+// //             max = arr[i];
+// //         }
+// //     }
+// //     console.log(max);
+// // }
+
+// // largest(num);
+
+// Question 10
+// // let num = [20,500,30,59,1000,5,];
+
+// // let largetsNum = num[0];
+// // let largest = (arr) => {
+// //     for(let i = 1; i < arr.length; i++){
+// //         if(arr[i] > largetsNum){
+// //             largetsNum = arr[i];
+// //         }
+// //     }
+// // }
+
+// // largest(num);
+// //     console.log(largetsNum);
+
+// Question 11
+// // let num = [20,500,30,59,1000,5,];
+
+// // let largetsNum = num[0];
+// // let largest = (arr) => {
+// //     for(let i = 1; i < arr.length; i++){
+// //         if(arr[i] > largetsNum){
+// //             largetsNum = arr[i];
+// //         }
+// //     }
+// // }
+
+// // largest(num);
+// // console.log(largetsNum);
+
+// Question 12
+// // let num = [20,500,30,59,1000,5,];
+// // let total = 0;
+// // let sum = (arr) => {
+// //     for(let i = 0; i < arr.length; i++){
+// //         total = arr[i] + total ;
+// //     }
+// // }
+
+// // sum(num);
+// // console.log(total);
+
+//  Question 13
+// // let vowels = ['a','e','i','o','u'];
+// // let countVowels = (arr) => {
+// //     console.log(arr.length);
+// // }
+// // countVowels(vowels);
+ 
+// Question 14
+let text =  "the smarter way     to    learn   javascript";
+
+console.log(text.replaceAll(" ", ""));
